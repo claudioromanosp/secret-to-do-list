81903b5d838ec69da4197f86dc8510a0d99b2aec
¨Claudio <claudio.romano@jolivi.com.br>
2023-12-14 18:04:23 -0300

    Feature: criando html do formulário e Hook useState

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,18 @@
+import React, { useState } from "react";
+
 
 function Home(){
-return <h1>Home</h1>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+return (
+  <div className="container">
+    <form className="form">
+      <input type="text" name="email" value={email} onChange={inputEmail} />
+      <input type="password" name="password" value={password} onChange={inputPassword} />
+    </form>
+  </div>
+);
 }
 
 export default Home;
