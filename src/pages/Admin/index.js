03e536212a44919758c8cf4747c24f4c1c9ea9c4
¨Claudio <claudio.romano@jolivi.com.br>
2023-12-19 11:44:38 -0300

    Feature: nova pagina de admin / rota de admin

---
+++ src/pages/Admin/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import './styles.css'
+
+function Admin(){
+  return (
+    <div className="container">
+      <h1>Admin</h1>
+    </div>
+  );
+}
+
+export default Admin;
