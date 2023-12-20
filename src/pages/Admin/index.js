a00f896361070cd3cc3cb59a269235f370242c43
¨Claudio <claudio.romano@jolivi.com.br>
2023-12-20 15:31:28 -0300

    Feature: formulário para criar tarefas

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,11 +1,36 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import './styles.css'
+import './styles.css';
 
 function Admin(){
+  const [task, setTask] = useState("")
+
+function inputTask(e){
+  e.preventDefault()
+    let input = e.target.value
+    setTask(input)   
+  }
+
+  function handleRegister(e) {
+    e.preventDefault();
+    let input = e.target.value;
+    setTask(input);
+  }
+
   return (
     <div className="container">
-      <h1>Admin</h1>
+      <h1>Minhas Tarefas</h1>
+
+
+      <form className="form" onSubmit={handleRegister}>
+        <textarea
+          placeholder="Digite sua tarefa ..."
+          value={inputTask}
+          onChange={inputTask}
+        ></textarea>
+        <button>Criar Tarefa</button>
+      </form>
+
     </div>
   );
 }
