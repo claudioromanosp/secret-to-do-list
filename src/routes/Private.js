ed7debfdfc7211ba469590086dbd57c413ead68a
¨Claudio <claudio.romano@jolivi.com.br>
2023-12-20 15:12:03 -0300

    Feature: página Admin modo privado

---
+++ src/routes/Private.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import { auth } from "../config";
+import { onAuthStateChanged } from "firebase/auth";
+
+function Private({ children}){
+    const [loading, setLoading] = useState(true);
+    const [signed, setSigned] = useState(true);
+    useEffect(()=>{
+        async function checkLogin(){
+            await onAuthStateChanged(auth, (user) => {
+                if(user){
+                    const userData = {
+                        uid: user.uid,
+                        email: user.email
+                    }
+                    localStorage.setItem("@detailUser", JSON.stringify(userData))
+                    setLoading(false);
+                    setSigned(true);
+                    
+                }else{
+                    setLoading(false);
+                    setSigned(false);
+                }
+            })
+        }
+        checkLogin();
+    },[])
+    if(loading){
+        return(<h1>loading ...</h1>)
+    }
+    if (!signed) {
+      return <Navigate to="/" />;
+    }
+    return children;
+}
+
+export default Private;
